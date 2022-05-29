ecf57cf2ec62b9530a597a4f6be35d034adf855f
Brahim Chahbouni <brahimcah@gmail.com>
2022-05-29 23:07:36 +0200

    update

--- src/main/java/com/brahimcah/uf5javafx/HelloController.java
+++ src/main/java/com/brahimcah/uf5javafx/HelloController.java
@@ -4,7 +4,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
-
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
@@ -18,15 +17,20 @@ public class HelloController {
 
     public void getDate(ActionEvent event) {
 
+        //Cream una variable tipus LocalDate el qual obtindra la data del myDatePicker
         LocalDate myDate = myDatePicker.getValue();
         String dia = myDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        /**Cream la variable tipo Sting amb el nom diaPrint
+         * el qual obte la date de myDate amb el format europeu de la data  * */
         String diaPrint = myDate.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
         System.out.println(diaPrint);
+        //Cream el objecte novaData i li enviem el dia com valor de referencia
         NovaDate novaDate = new NovaDate(dia);
         String diaSemana = novaDate.diaSemana();
         //Cream un Sting el crearem un text amb el día i el dia de la
         // semana per que es imprimexi en pantalla mes endevant
         String textFinal = "El dia " + diaPrint + " es un " + diaSemana;
+        //Enviem les dades del String textFinal en el myLabel
         myLabel.setText(textFinal);
 
 
--- src/main/java/com/brahimcah/uf5javafx/NovaDate.java
+++ src/main/java/com/brahimcah/uf5javafx/NovaDate.java
@@ -20,20 +20,22 @@ public class NovaDate {
     }
 
     public String diaSemana(){
-
+        //Cream la variable Valor_dia el qual guardarem el dia de la semana en format String
         String Valor_dia = null;
         SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-        Date fechaActual = null;
+        Date dateActual = null;
         try {
-            fechaActual = df.parse(dia);
+            dateActual = df.parse(dia);
 
         } catch (ParseException e) {
             System.err.println("No se ha podido parsear la fecha.");
             e.printStackTrace();
         }
-        GregorianCalendar fechaCalendario = new GregorianCalendar();
-        fechaCalendario.setTime(fechaActual);
-        var diaSemana = fechaCalendario.get(Calendar.DAY_OF_WEEK);
+        GregorianCalendar dateCalendari = new GregorianCalendar();
+        dateCalendari.setTime(dateActual);
+        // La variable diaSemana el que fa es del 1 al 7 saber quin dia cau el dia de la setmana
+        // de la variable dateCalendari segons el calandrai Greogria (Que es el que fem servir)
+        var diaSemana = dateCalendari.get(Calendar.DAY_OF_WEEK);
 
         /**
          * El switch el que ens adjuadar es amb la variable diaSemana segons el resultat numeric
